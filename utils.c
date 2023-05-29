a71c4221a4a5a6b80d7da3cf702732f4cced85d3
Serahnams <128810177+Serahnams@users.noreply.github.com>
2023-05-29 18:30:37 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -10,7 +10,7 @@ int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
-
+	
 	return (0);
 }
 
@@ -34,7 +34,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
+	
 	return (3);
 }
 
@@ -63,7 +63,6 @@ long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
-	
 	else if (size == S_SHORT)
 		return ((short)num);
 
