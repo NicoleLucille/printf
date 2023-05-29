d638e43fc319ab01ee3c351af1c174d8f0a45582
Serahnams <128810177+Serahnams@users.noreply.github.com>
2023-05-29 15:58:09 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -25,6 +25,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
+	
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -62,6 +63,7 @@ long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
+	
 	else if (size == S_SHORT)
 		return ((short)num);
 
